55363a346aaefed9aac86483d908cd1a4f474e4a
CTID <CTID@windows10.microdone.cn>
2020-04-01 20:25:42 +0800

    创建

---
+++ src/main/java/com/phx/activemq/topic/Consumer.java
@@ -0,0 +1,43 @@
+package com.phx.activemq.topic;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.DeliveryMode;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+public class Consumer {
+	
+	public static void main(String[] args) throws JMSException {
+		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(ActiveMQConnection.DEFAULT_USER, ActiveMQConnection.DEFAULT_PASSWORD, "tcp://140.143.26.129:61616");
+		Connection connection =   connectionFactory.createConnection();
+		connection.start();
+		Session session = connection.createSession(Boolean.FALSE, Session.AUTO_ACKNOWLEDGE);
+		Destination destination = session.createTopic("ww-activemq_topic");
+		MessageConsumer messageConsumer  = session.createConsumer(destination);
+		
+		while(true) {
+			TextMessage textMessage = (TextMessage) messageConsumer.receive();
+			if(textMessage!=null) {
+				String s = textMessage.getText();
+				System.out.println("消费者 获取到消息：text:"+s);
+			}else {
+				break;
+			}
+			
+		}
+		System.out.println("消费者 获取消息完毕！！");
+			
+		
+	}
+	
+	
+
+}
---
+++ src/main/java/com/phx/activemq/topic/Producer.java
@@ -0,0 +1,40 @@
+package com.phx.activemq.topic;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.DeliveryMode;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+public class Producer {
+	public static void main(String[] args) throws JMSException {
+		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(ActiveMQConnection.DEFAULT_USER, ActiveMQConnection.DEFAULT_PASSWORD, "tcp://140.143.26.129:61616");
+		Connection connection =   connectionFactory.createConnection();
+		connection.start();
+		Session session = connection.createSession(Boolean.FALSE, Session.AUTO_ACKNOWLEDGE);
+		Destination destination = session.createTopic("ww-activemq_topic");
+		MessageProducer producer  = session.createProducer(destination);
+		producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+		for(int i = 1;i<=5;i++) {
+			String ms = "我是生产者："+i;
+			System.out.println(ms);
+			sendMsg(session,producer, ms);
+		}
+		System.out.println("生产者 发送消息完毕！！");
+			
+		
+	}
+	
+	
+	 public static void sendMsg(Session session,MessageProducer producer,String message) throws JMSException {
+		TextMessage textMessage = session.createTextMessage(message);
+		producer.send(textMessage);
+	}
+
+}
